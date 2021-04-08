d6a77e930c81f1620b692c591affbf1268a8c718
邓南平 <nanping.deng@dhc.com.cn>
2021-04-08 23:33:39 +0800

    后台

---
+++ src/main/java/com/bjpowernode/finance/entity/OutAndIn.java
@@ -0,0 +1,26 @@
+package com.bjpowernode.finance.entity;
+
+/**
+ * @since 2021/4/8 20:40
+ */
+public class OutAndIn {
+  private String outMoney;
+
+  private String inMoney;
+
+  public String getOutMoney() {
+    return outMoney;
+  }
+
+  public void setOutMoney(String outMoney) {
+    this.outMoney = outMoney;
+  }
+
+  public String getInMoney() {
+    return inMoney;
+  }
+
+  public void setInMoney(String inMoney) {
+    this.inMoney = inMoney;
+  }
+}
